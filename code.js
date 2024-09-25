f88edf1c3117677b68e2ea4e7a0d2d4991883cbf
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-09-25 15:48:40 -0600

    First commit

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length == 1) {
+        return a[0];
+    }
+    if (a.length == 0) {
+        return 0;
+    }
+    const size = a.length;
+    
+    const subArrLength = Math.floor(size / 3);
+    const leftEnd = subArrLength;
+    const middleEnd = 2 * subArrLength;
+
+    const leftSubarray = a.slice(0, leftEnd);
+    const middleSubarray = a.slice(leftEnd, middleEnd);
+    const rightSubarray = a.slice(middleEnd);
+
+    const leftArrSum = divideAndConquerSum(leftSubarray);
+    const middleArrSum = divideAndConquerSum(middleSubarray);
+    const rightArrSum = divideAndConquerSum(rightSubarray);
+
+    // console.log("leftArrSum: " + leftArrSum);
+    // console.log("middleArrSum: " + middleArrSum);
+    // console.log("rightArrSum: " + rightArrSum);
+    // console.log("Full Sum: " + (leftArrSum + middleArrSum + rightArrSum));
+
+
+    return leftArrSum + middleArrSum + rightArrSum;
 }
